33114cd7b49df1580878338c863efb57ca5368c9
Andrew Brandt <brandt.andrew89@gmail.com>
2016-12-02 19:05:57 -0700

    Added calculate.py and basic comments

---
+++ calculate.py
@@ -0,0 +1,13 @@
+# Calculate the passer rating of a quarterback
+
+# Manually change these numbers:
+
+# Do the calculation here:
+# 	There are 4 parts
+#	Part 1
+
+#	Part 2
+
+# 	Part 3
+
+# 	Part 4

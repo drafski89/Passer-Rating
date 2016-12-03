9708bedae4bf7fe039b6ed4262511dfdf7b8ac92
Andrew Brandt <brandt.andrew89@gmail.com>
2016-12-02 19:17:34 -0700

    Wrote calculate.py. Need to format the output to 2 decimal places

--- calculate.py
+++ calculate.py
@@ -1,13 +1,32 @@
 # Calculate the passer rating of a quarterback
+# Python 3, but cover the option for 2.6+
+from __future__ import print_function
 
-# Manually change these numbers:
+# Manually change these numbers
+# Note: These must be floats
+completions = 30.0
+attempts = 39.0
+passingYards = 313.0
+touchdowns = 2.0
+interceptions = 0.0
 
 # Do the calculation here:
-# 	There are 4 parts
-#	Part 1
+# 	There are 5 parts
+#	Part 1: ((Completions / Attempts) - 0.3) * 5
+part1 = ((completions / attempts) - 0.3) * 5
 
-#	Part 2
+#	Part 2: ((Passing Yards / Attempts) - 3) * 0.25
+part2 = ((passingYards / attempts) - 3) * 0.5
 
-# 	Part 3
+# 	Part 3: (Touchdowns / Attempts) * 20
+part3 = (touchdowns / attempts) * 20
 
-# 	Part 4
+# 	Part 4: 2.375 - ((Interceptions / Attempts) * 25)
+part4 = 2.375 - ((interceptions / attempts) * 25)
+
+# 	Part 5: Final Calculation
+#	((Part1 + Part2 + Part3 + Part4) / 6) * 100
+finalCalculation = ((part1 + part2 + part3 + part4) / 6) * 100
+
+#	Print the result to the user
+print("The passer rating is: " + str(finalCalculation))
